04fe8c7cbaf15023594d423b84a6214b87fadbe2
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-12-21 15:23:58 +0200

    header file sort

--- sort.h
+++ sort.h
@@ -11,13 +11,13 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
@@ -34,6 +34,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
 
 #endif /*SORT_H*/
