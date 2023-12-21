989132282351047e189d556d3864348a1e0c9ad1
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-12-21 16:21:52 +0200

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: element to be sorted
+ * @size: size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int i, j, min_idx;
+	int temp = *;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < n; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+
+		if (min_idx != i)
+		{
+			*&array[min_idx] = *&array[i];
+			*&array[i] = temp;
+		}
+		print_array(array, size);
+	}
+}
