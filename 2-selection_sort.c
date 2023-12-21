9b4d4e0952929cad2ad7af3b15f661f78ed4e84a
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-12-21 16:24:22 +0200

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,14 +15,16 @@ void selection_sort(int *array, size_t size)
 	{
 		min_idx = i;
 		for (j = i + 1; j < n; j++)
+		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
 
-		if (min_idx != i)
-		{
-			*&array[min_idx] = *&array[i];
-			*&array[i] = temp;
-			print_array(array, size);
+			if (min_idx != i)
+			{
+				*&array[min_idx] = *&array[i];
+				*&array[i] = temp;
+				print_array(array, size);
+			}
 		}
 	}
 }
